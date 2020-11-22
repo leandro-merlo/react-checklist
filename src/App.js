31d9318011f8dd454235b0698c408139ebc44759
Leandro Manzano Merlo <leandro.merlo@gmail.com>
2020-11-21 23:01:40 -0300

    Primeiro commit - configuração inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import { Button } from 'reactstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <Button color="primary">Button 1</Button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
